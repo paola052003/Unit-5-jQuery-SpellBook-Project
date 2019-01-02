f7eb56f95eea44665dacc1afd1498f6b0e458cfa
paola052003 <43606931+paola052003@users.noreply.github.com>
2019-01-02 08:27:02 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("img").click(function(){
+    $(".ball").fadeToggle();
+});
+
+
+
+//Spell #2
+
+$("   img   ").click(function(){
+    $(".pail").slideToggle();
+});
+
+
+
+//Spell #3
+
+$("img ").click(function(){
+    $(".battery").hide();
+});
+
+
+
